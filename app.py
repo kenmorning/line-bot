56a37f4e6451c5da0a138abe536b1eeb65c4f0d3
Ken <ikenaero@hotmail.com>
2023-01-04 19:54:08 +0800

    Update text and sticker

--- app.py
+++ app.py
@@ -37,9 +37,32 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+     msg = event.message.text 
+    r = '很抱歉，你說什麼'
+    
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='2',
+            sticker_id='23'
+        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return # 回傳空值，不執行下面
+
+    if msg in ['hi', 'Hi'] # 用清單檢查
+        r = '嗨'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg: # 訂位出現在訊息中的話
+        r = '您想訂位，是嗎？'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
